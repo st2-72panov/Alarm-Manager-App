a49320f3c79232289cfa6d4bfbffcf666e256327
PerfectCroissant <kat.ste2016@yandex.ru>
2024-06-08 23:02:14 +0300

    outlined text field

--- app/src/main/java/com/example/alarmmanagerapp/ui/page_solo/SolosEditingDialog.kt
+++ app/src/main/java/com/example/alarmmanagerapp/ui/page_solo/SolosEditingDialog.kt
@@ -5,31 +5,25 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.isImeVisible
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
-import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
@@ -90,30 +84,21 @@ fun SolosEditingDialog(
 
             Spacer(Modifier.height(5.dp))
 
-            TextField(
-                modifier = Modifier.fillMaxWidth(),
+            OutlinedTextField(
                 shape = RoundedCornerShape(16.dp),
-                colors = TextFieldDefaults.colors(
+                colors = OutlinedTextFieldDefaults.colors(
                     focusedContainerColor = AppColor.dimmest,
                     unfocusedContainerColor = AppColor.dimmest,
                     focusedTextColor = AppColor.light,
                     unfocusedTextColor = AppColor.dim,
                     cursorColor = AppColor.contrast,
-                    focusedIndicatorColor = AppColor.contrast
+                    focusedBorderColor = AppColor.contrast
                 ),
                 maxLines = 3,
                 value = editingEntity.title,
-                onValueChange = { editingEntity = editingEntity.copy(title = it) }
+                onValueChange = { editingEntity = editingEntity.copy(title = it) },
+                placeholder = { Text("Title") }
             )
-
-//            BasicTextField(
-//                modifier = Modifier.fillMaxWidth(),
-//                value = editingEntity.title,
-//                onValueChange = { editingEntity = editingEntity.copy(title = it) },
-//                maxLines = 3,
-//                textStyle = TextStyle(color = AppColor.light),
-//                cursorBrush = Brush.linearGradient(colors = listOf(AppColor.light))
-//            )
 
             Row(
                 Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
@@ -125,32 +110,6 @@ fun SolosEditingDialog(
                 TextButton(onClick = { onSave(editingEntity) }) {
                     Text("Сохранить", color = AppColor.light)
                 }
-            }
-        }
-    }
-}
-
-
-@OptIn(ExperimentalLayoutApi::class)
-fun Modifier.clearFocusOnKeyboardDismiss(): Modifier = composed {
-    var isFocused by remember { mutableStateOf(false) }
-    var keyboardAppearedSinceLastFocused by remember { mutableStateOf(false) }
-    if (isFocused) {
-        val imeIsVisible = WindowInsets.isImeVisible
-        val focusManager = LocalFocusManager.current
-        LaunchedEffect(imeIsVisible) {
-            if (imeIsVisible) {
-                keyboardAppearedSinceLastFocused = true
-            } else if (keyboardAppearedSinceLastFocused) {
-                focusManager.clearFocus()
-            }
-        }
-    }
-    onFocusEvent {
-        if (isFocused != it.isFocused) {
-            isFocused = it.isFocused
-            if (isFocused) {
-                keyboardAppearedSinceLastFocused = false
             }
         }
     }
